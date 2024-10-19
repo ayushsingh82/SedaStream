309a9fc18efd4bec8fd3d6eb7c801dba87679cdd
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-10-20 00:21:41 +0530

    home

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function Navbar() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+      <nav className=''>
+        <div className='flex flex-row mx-auto px-[40px] py-[25px] justify-between items-center mt-[0px] bg-[#1F1B30]'>
+          <div className='font-bold text-2xl text-white'>
+            <a href='/'>Seda</a>
+          </div>
+
+          {/* Dropdown button */}
+          <div className='relative'>
+            <button
+              onClick={toggleDropdown}
+              className='bg-black text-white font-bold text-black px-4 py-2 rounded-md'>
+             Get Started
+            </button>
+
+            {/* Dropdown Menu */}
+            {dropdownOpen && (
+              <div className='absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg'>
+                <a
+                  href='polkadotlayout'
+                  className='block px-4 py-2 text-black hover:bg-gray-200'>
+                Dropdown 1
+                </a>
+                <a
+                  href=''
+                  className='block px-4 py-2 text-black hover:bg-gray-200'>
+                Dropdown 2
+                </a>
+             
+              </div>
+            )}
+          </div>
+        </div>
+        <hr className='border-t-2 border-black' />
+      </nav>
+    );
+}
+
+export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter,RouterProvider,createRoutesFromElements} from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Navbar from './components/Navbar.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+    <Route path='/' element={<Home/>}/>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+   <Navbar/>
+   <RouterProvider router={router}/>
+
+  </React.StrictMode>,
 )
